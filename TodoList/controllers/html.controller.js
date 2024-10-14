759695ae0d421251bdf08a68598de37b74515be5
CopperKim <77cubeking@gmail.com>
2024-10-14 19:48:48 +0900

    m

--- TodoList/controllers/html.controller.js
+++ TodoList/controllers/html.controller.js
@@ -1,9 +1,10 @@
 const htmlController = {}
 const { createTask, getTasks } = require("../controllers/task.controller")
+const path = require("path")
 
 htmlController.gethtml = async(req, res) => {
     try{
-        res.sendFile(__dirname + '../htmls/index.html');
+        res.sendFile(path.join(__dirname, '../htmls/index.html'));
     }catch(error){
         return res.status(400).json({status:"html file send fail", error});
     }
