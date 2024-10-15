4d13ac21cde21cd4cc4a95e9192b24a9d9649a2c
CopperKim <77cubeking@gmail.com>
2024-10-15 23:06:14 +0900

    으아아

--- C_C++/test.cpp
+++ C_C++/test.cpp
@@ -1,23 +1,62 @@
-#include <iostream> 
-#include <stack>
+#include <iostream>
+// #include <vector>
+#include <deque>
+#include <stdio.h>
+// #include <stack>
 
 using namespace std;
 
 int main() {
-    int N; cin >> N;
-    for(int i=0;i<N;i++) {
-        stack<char> S;
-        char input[55];
-        cin >> input;
-        for(int j=0;input[j]!='\0';j++) {
-            if(input[j] == '(') S.push('(');
-            else if (S.empty()) {
-                S.push('h');
-                break;
-            } 
-            else S.pop();
+    int T; cin >> T;
+    for(int i=0;i<T;i++) {
+        char p[100003];
+        cin >> p;
+
+        bool isLeft = true;
+        int j, len;
+
+        deque<char> nums;
+        cin >> len;
+        getchar();
+        for(j=0;j<len;j++) {
+            getchar();
+            nums.push_front(getchar());
         }
-        if (S.empty()) cout << "YES" << '\n';
-        else cout << "NO" << '\n';
-    }
+
+        j=0;
+
+        while(p[j] != '\0') {
+            if (nums.empty()) {
+                cout << "error\n";
+                break;
+            }
+            else if (p[j] == 'D') {
+                if (isLeft) nums.pop_back();
+                else nums.pop_front();
+            }
+            else {
+                isLeft = !isLeft;
+            }
+            j++;
+        }
+        
+        if(nums.empty()) continue;
+
+        cout << '[';
+        if (isLeft) {
+            while (!nums.empty()) {
+                cout << nums.back();
+                nums.pop_back();
+                if (!nums.empty()) cout << ',';
+                else cout << "]\n";
+            } 
+        } else {
+            while (!nums.empty()) {
+                cout << nums.front();
+                nums.pop_front();
+                if (!nums.empty()) cout << ',';
+                else cout << "]\n";
+            } 
+        }
+    }   
 }
