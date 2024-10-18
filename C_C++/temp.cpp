f493fdeb02a255ff5f5b045406c9227d39254f0d
김시유 <77cubeking@gmail.com>
2024-10-19 03:38:58 +0900

    2842 added

--- C_C++/temp.cpp
+++ C_C++/temp.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
 int main () {
-    int j=0, len, index = 1; cin >> len;
-    vector<vector<char>> nums;
-    string input; cin >> input;
-
-    for(j=0;j<len;j++) {
-        vector<char> addnum;
-        while(input[index] != ',' && input[index] != ']') {
-            addnum.push_back(input[index]);
-            index++;
-        }
-        nums.push_back(addnum);
-        index++;
-    }
-    
-    for(j=0;j<len;j++) {
-        for(char ch : nums[j]) {
-            cout << ch << ' ';
-        }
-    }
-
-    return 0;
+    queue<int> Q;
+    Q.push(1);
+    Q.push(2);
+    Q.push(3);
+    Q.push(4);
+    cout << Q.front() << Q.back();
 }
