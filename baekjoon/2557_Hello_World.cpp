06828375330d2e43e67922c0f63935b8a90a0f2d
CopperKim <copperkim29@gmail.com>
2025-06-18 21:50:46 +0900

    i dont know what has been changed so i have no idea if it will be fukced up

--- baekjoon/2557_Hello_World.cpp
+++
@@ -1,9 +0,0 @@
-// https://www.acmicpc.net/problem/2557
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Hello World!";
-}
---
+++ data_structure/List/ArrayList/DynamicArrayList 복사본.java
@@ -0,0 +1,70 @@
+package data_structure.List.ArrayList;
+
+import data_structure.List.List;
+
+public class DynamicArrayList<E> implements List<E> {
+    
+    private static int CAPACITY = 16;
+    private E[] array;
+    private int size=0;
+
+    public DynamicArrayList() { array = (E[]) new Object[CAPACITY]; }
+
+    protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= n) throw new IndexOutOfBoundsException("illegal index : " + i);
+    }
+
+    public int size() { return size; }
+
+    public boolean isEmpty() { return size == 0; } 
+
+    public void resize(int cap) {
+        E[] newArr = (E[]) new Object[cap];
+        for(int i=0;i<size;i++) newArr[i] = array[i];
+        array = newArr;
+    } 
+
+    public E get(int i) throws IndexOutOfBoundsException { 
+        checkIndex(i, size); 
+        return array[i]; 
+    }
+
+    public E set(int i, E e) throws IndexOutOfBoundsException { 
+        checkIndex(i, size);
+        E res = array[i];
+        array[i] = e;
+        return res;
+    }
+
+    public void add(int i, E e) throws IndexOutOfBoundsException, IllegalStateException {
+        checkIndex(i, size+1);
+        if (size == CAPACITY) {
+            resize(CAPACITY = 2*CAPACITY);
+        }
+        for(int k = size++;k>i;k--) {
+            array[k] = array[k-1];
+        }
+        array[i] = e;
+        return;
+    }
+
+    public E remove(int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E res = array[i];
+        for(;i<size;i++) {
+            array[i] = array[i+1];
+        }
+        size--;
+        if (size == CAPACITY/4) resize(CAPACITY = CAPACITY/2);
+        return res;
+    }
+
+    public String toString() {
+        String res = "";
+        for(int i=0;i<size;i++) {
+            res += array[i].toString() + " ";
+        }
+        res += ", size : " + size + ", capacity : " + CAPACITY;
+        return res;
+    }
+}
